16b5822dcb8ad145f3f30a2f2e1632dd7f9d265c
Mohsen <kh.mossen@gmail.com>
2020-04-05 10:52:15 +1000

    Improving performance

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
 import Styled from "styled-components";
 import Loading from "../Loading";
@@ -17,21 +17,28 @@ const List = (props) => {
     }
   };
 
+  /*
+  * Improving performance to reduce calling this
+  * function when items remains unchanged
+  */
+  const itemElements = useMemo(
+    () => items && items.map((user) => (
+      <Item
+        key={user.id}
+        avatar={user.avatar}
+        fullName={`${user.first_name} ${user.last_name}`}
+      />
+    )),
+    [items]
+  );
+
   return (
     <div className="flex justify-center">
       <Card
         onScroll={handleScroll}
         className="m-2 mt-10 max-w-full bg-white rounded overflow-scroll shadow-lg"
       >
-        {
-          items && items.map((user) => (
-            <Item
-              key={user.id}
-              avatar={user.avatar}
-              fullName={`${user.first_name} ${user.last_name}`}
-            />
-          ))
-        }
+        {itemElements}
         <Loading show={isLoading} className="mb-4" />
       </Card>
     </div>
